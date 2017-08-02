d1b703e10bf1fe54d8f199172600ca23e72f2742
MLEINC <michael.lucke@incowia.com>
2017-08-02 10:48:55 +0200

    shortened code

--- pathfinder/vtables/appmap2bca/src/Report.js
+++ pathfinder/vtables/appmap2bca/src/Report.js
@@ -14,7 +14,6 @@ class Report extends Component {
 			setup: null,
 			data: []
 		};
-
 	}
 
 	componentDidMount() {
@@ -30,12 +29,7 @@ class Report extends Component {
 		lx.executeGraphQL(CommonQueries.tagGroups).then((tagGroups) => {
 			const index = new DataIndex();
 			index.put(tagGroups);
-			let appMapID = index.getTags('BC Type', 'AppMap');
-			if (appMapID.length > 0) {
-				appMapID = appMapID[0].id;
-			} else {
-				appMapID = undefined;
-			}
+			const appMapID = index.getFirstTagID('BC Type', 'AppMap');
 			lx.executeGraphQL(this._createQuery(appMapID)).then((data) => {
 				index.put(data);
 				this._handleData(index, appMapID);
@@ -58,80 +52,64 @@ class Report extends Component {
 			tagNameDef = '';
 		}
 		return `{businessCapabilities: allFactSheets(
-						sort: {mode: BY_FIELD, key: "displayName", order: asc}, 
-						filter: {facetFilters: [
-							{facetKey: "FactSheetTypes", keys: ["BusinessCapability"]}
-							${appMapIDFilter}
-						]}
-					) {
-						edges { node {
-							id fullName ${tagNameDef}
-							... on BusinessCapability {
-								relToParent {
-									edges { node { factSheet { id fullName }}}
+					sort: {mode: BY_FIELD, key: "displayName", order: asc}, 
+					filter: {facetFilters: [
+						{facetKey: "FactSheetTypes", keys: ["BusinessCapability"]}
+						${appMapIDFilter}
+					]}
+				) {
+					edges { node {
+						id fullName ${tagNameDef}
+						... on BusinessCapability {
+							relToParent { edges { node { factSheet { id fullName } } } }
+							relBusinessCapabilityToBCA { edges { node { factSheet { id fullName
+								... on BusinessCapability {
+									relToParent { edges { node { factSheet {id fullName
+										... on BusinessCapability {
+											relToParent { edges { node { factSheet {id fullName
+												... on BusinessCapability {
+													relToParent { edges { node { factSheet { id fullName } } } }
+												}
+											}}}}
+										}
+									}}}}
 								}
-								relBusinessCapabilityToBCA { edges { node { factSheet { id fullName
-									... on BusinessCapability {
-										relToParent { edges { node { factSheet {id fullName
-											... on BusinessCapability {
-												relToParent { edges { node { factSheet {id fullName
-													... on BusinessCapability {
-														relToParent { edges { node { factSheet { id fullName }}}}
-													}
-												}}}}
-											}
-										}}}}
-									}
-								}}}}
-							}
-						}}
-					}
-				}`;
+							}}}}
+						}
+					}}
+				}}`;
 	}
 
 	_handleData(index, appMapID) {
 		const tableData = [];
 		index.businessCapabilities.nodes.forEach((appMapL2) => {
-			if (appMapID || (!appMapID && index.includesTag(appMapL2, 'AppMap'))) {
-				if (appMapL2.relBusinessCapabilityToBCA) {
-					appMapL2.relBusinessCapabilityToBCA.nodes.forEach((bcaL4) => {
-						let appMapL1 = undefined;
-						if (appMapL2.relToParent) {
-							appMapL1 = appMapL2.relToParent.nodes[0];
-						}
-						
-						let bcaL3 = undefined;
-						if (bcaL4.relToParent) {
-							bcaL3 = bcaL4.relToParent.nodes[0];
-						}
-						
-						let bcaL2 = undefined;
-						if (bcaL3 && bcaL3.relToParent) {
-							bcaL2 = bcaL3.relToParent.nodes[0];
-						}
-						
-						let bcaL1 = undefined;
-						if (bcaL2 && bcaL2.relToParent) {
-							bcaL1 = bcaL2.relToParent.nodes[0];
-						}
-						
-						tableData.push({
-							appMapL1ID: appMapL1 && appMapL1.id ? appMapL1.id : '',
-							appMapL1Name: appMapL1 && appMapL1.fullName ? appMapL1.fullName : '',
-							appMapL2ID: appMapL2.id,
-							appMapL2Name: appMapL2.fullName,
-							bcaL1ID: bcaL1 && bcaL1.id ? bcaL1.id : '',
-							bcaL1Name: bcaL1 && bcaL1.fullName ? bcaL1.fullName : '',
-							bcaL2ID: bcaL2 && bcaL2.id ? bcaL2.id : '',
-							bcaL2Name: bcaL2 && bcaL2.fullName ? bcaL2.fullName : '',
-							bcaL3ID: bcaL3 && bcaL3.id ? bcaL3.id : '',
-							bcaL3Name: bcaL3 && bcaL3.fullName ? bcaL3.fullName : '',
-							bcaL4ID: bcaL4.id,
-							bcaL4Name: bcaL4.fullName,
-						});
-					});
-				}
+			if (!appMapID && !index.includesTag(appMapL2, 'AppMap')) {
+				return;
 			}
+			const subIndex = appMapL2.relBusinessCapabilityToBCA;
+			if (!subIndex) {
+				return;
+			}
+			subIndex.nodes.forEach((bcaL4) => {
+				const appMapL1 = appMapL2.relToParent ? appMapL2.relToParent.nodes[0] : undefined;
+				const bcaL3 = bcaL4.relToParent ? bcaL4.relToParent.nodes[0] : undefined;
+				const bcaL2 = bcaL3 && bcaL3.relToParent ? bcaL3.relToParent.nodes[0] : undefined;
+				const bcaL1 = bcaL2 && bcaL2.relToParent ? bcaL2.relToParent.nodes[0] : undefined;
+				tableData.push({
+					appMapL1ID: appMapL1 && appMapL1.id ? appMapL1.id : '',
+					appMapL1Name: appMapL1 && appMapL1.fullName ? appMapL1.fullName : '',
+					appMapL2ID: appMapL2.id,
+					appMapL2Name: appMapL2.fullName,
+					bcaL1ID: bcaL1 && bcaL1.id ? bcaL1.id : '',
+					bcaL1Name: bcaL1 && bcaL1.fullName ? bcaL1.fullName : '',
+					bcaL2ID: bcaL2 && bcaL2.id ? bcaL2.id : '',
+					bcaL2Name: bcaL2 && bcaL2.fullName ? bcaL2.fullName : '',
+					bcaL3ID: bcaL3 && bcaL3.id ? bcaL3.id : '',
+					bcaL3Name: bcaL3 && bcaL3.fullName ? bcaL3.fullName : '',
+					bcaL4ID: bcaL4.id,
+					bcaL4Name: bcaL4.fullName,
+				});
+			});
 		});
 		this.setState({
 			data: tableData
@@ -141,45 +119,60 @@ class Report extends Component {
 	/* formatting functions for the table */
 
 	_formatLink(cell, row, idName) {
-		let id = row[idName];
-		if (!cell || !id) {
+		if (!cell) {
 			return '';
 		}
-		return (<Link link={'factsheet/BusinessCapability/' + id} target='_blank' text={cell} />);
+		return (<Link link={'factsheet/BusinessCapability/' + row[idName]} target='_blank' text={cell} />);
 	}
 
 	render() {
 		if (this.state.data.length === 0) {
 			return null;
-		}		
+		}
 		return (
 			<BootstrapTable data={this.state.data} keyField='appMapL2ID'
-				striped hover search pagination ignoreSinglePage exportCSV 
+				striped hover search pagination ignoreSinglePage exportCSV
 				options={{ clearSearch: true }}>
+				<TableHeaderColumn hidden export
+					 dataField='appMapL1ID'
+					 csvHeader='appmap-L1-id'
+				>appMapL1ID</TableHeaderColumn>
 				<TableHeaderColumn dataSort
 					dataField='appMapL1Name'
 					dataAlign='left'
 					dataFormat={this._formatLink}
 					formatExtraData={'appMapL1ID'}
-					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 					csvHeader='appmap-L1'
+					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 				>App Map L1</TableHeaderColumn>
+				<TableHeaderColumn hidden export
+					 dataField='appMapL2ID'
+					 csvHeader='appmap-L2-id'
+				>appMapL2ID</TableHeaderColumn>
 				<TableHeaderColumn dataSort
 					dataField='appMapL2Name'
 					dataAlign='left'
 					dataFormat={this._formatLink}
 					formatExtraData={'appMapL2ID'}
-					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 					csvHeader='appmap-L2'
+					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 				>App Map L2</TableHeaderColumn>
+				<TableHeaderColumn hidden export
+					 dataField='bcaL1ID'
+					 csvHeader='bca-L1-id'
+				>bcaL1ID</TableHeaderColumn>
 				<TableHeaderColumn dataSort
 					dataField='bcaL1Name'
 					dataAlign='left'
 					dataFormat={this._formatLink}
 					formatExtraData={'bcaL1ID'}
-					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 					csvHeader='bca-L1'
+					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 				>BCA L1</TableHeaderColumn>
+				<TableHeaderColumn hidden export
+					 dataField='bcaL2ID'
+					 csvHeader='bca-L2-id'
+				>bcaL2ID</TableHeaderColumn>
 				<TableHeaderColumn dataSort
 					dataField='bcaL2Name'
 					dataAlign='left'
@@ -187,22 +180,31 @@ class Report extends Component {
 					formatExtraData={'bcaL2ID'}
 					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 					csvHeader='bca-L2'
+					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 				>BCA L2</TableHeaderColumn>
+				<TableHeaderColumn hidden export
+					 dataField='bcaL3ID'
+					 csvHeader='bca-L3-id'
+				>bcaL3ID</TableHeaderColumn>
 				<TableHeaderColumn dataSort
 					dataField='bcaL3Name'
 					dataAlign='left'
 					dataFormat={this._formatLink}
 					formatExtraData={'bcaL3ID'}
-					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 					csvHeader='bca-L3'
+					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 				>BCA L3</TableHeaderColumn>
+				<TableHeaderColumn hidden export
+					 dataField='bcaL4ID'
+					 csvHeader='bca-L4-id'
+				>bcaL4ID</TableHeaderColumn>
 				<TableHeaderColumn dataSort
 					dataField='bcaL4Name'
 					dataAlign='left'
 					dataFormat={this._formatLink}
 					formatExtraData={'bcaL4ID'}
-					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 					csvHeader='bca-L4'
+					filter={{ type: 'TextFilter', placeholder: 'Please enter a value' }}
 				>BCA L4</TableHeaderColumn>
 			</BootstrapTable>
 		);
